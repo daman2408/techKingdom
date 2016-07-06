3b3c407112531e8e00be9050493b7ae94c673a34
daman2408 <damanvir32@gmail.com>
2016-07-06 17:42:21 -0400

    video added

--- _site/js/app.js
+++ _site/js/app.js
@@ -51,13 +51,13 @@ app.controller('myCtrl', ['$scope', '$http', 'ngCart', '$routeParams', function(
     };
     $scope.products = [{
         name: "Avov IPTV Box",
-        image: ["images/avov-front.png", "images/Capture12.png", "images/frontrcu.png"],
+        image: ["images/avov-front.png", "images/Capture12.PNG", "images/frontrcu.png"],
         price: 150,
         description: "TVonline+ is our premium IPTV/OTT Android set top box. With our manufacturing business in Honk Kong, R&D business in Korea and a sales office in Toronto, Canada and Los Angeles, USA. Over the past couple of years, we have put together a dedicated team that specializes in the design production and maintenance of our hardware and middleware support for the IPTV/OTT set top boxes we manufacture. We target operators in the market who use and provide dedicated over IP-based networks (IPTV, OTT, VOD). This device comes pre-loaded with our Nova Middleware for easy convenience and deployment for the end-user. With more ram, strong processor it allows you to enhance the velocity of the set top box. The benefits of HEVC technology used in TVonline+ is that it requires smaller network bandwidth for provision of high quality video. TVonline+ comes equipped with a built-in WiFi and the ability to turn the STB into a internet hotspot. This unit comes with a remote control, HDMI cable, Power Supply, and an Owner's Manual.",
         info: "Please note: there will be no subscriptions, activations, TV channels or any other audio/video content provided with this unit. If you want to activate the box with a subscription that we currently offer, click here!. If you want to activate with someone else, that is fine too!"
     }, {
         name: "Android TV Box",
-        image: ["images/android.png", "images/3.jpg", "images/4.jpg"],
+        image: ["images/android.png", "images/3.JPG", "images/4.JPG"],
         price: 150,
         description: "The Xgody is a digital media streamer that provides quality video and audio capture from analogue sources. This model connects to other devices through Ethernet, Wi-Fi, USB and Bluetooth interfaces and is suitable for mediating audio and video signals. Its maximum resolution of 1080p lets you enjoy clear, bright text, and visual detail. This unit comes with a remote control, HDMI cable, Power Supply, and an Owner's Manual.",
         info: "Please note: there will be no subscriptions, activations, TV channels or any other audio/video content provided with this unit. If you want to activate the box with a subscription that we currently offer, click here!. If you want to activate with someone else, that is fine too!"
